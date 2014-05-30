f121045cb606a220287a634a24cb1b4b1c3f9541
Ji-Ye <ji-ye@Ji-Yeteki-MacBook-Pro.local>
2014-05-30 16:27:57 +0800

    Initial Commit

---
+++ final/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  final
+//
+//  Created by Ji-Ye on 14/5/30.
+//  Copyright (c) 2014年 Ji-Ye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ final/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  final
+//
+//  Created by Ji-Ye on 14/5/30.
+//  Copyright (c) 2014年 Ji-Ye. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
